b1d2b39db439e77c4f4c6f247c2e92df5d4df9e2
Agustin <agustin.ribotta.r@gmail.com>
2023-12-02 22:52:49 +0100

    Tipos de rutas

---
+++ main.py
@@ -0,0 +1,39 @@
+from typing import Union
+from fastapi import FastAPI
+
+# Create app
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    """ Prueba Hello Word """
+    return {
+        'Hello': 'World',
+        }
+
+
+@app.get('/hola')
+def hola_mundo():
+    """ Prueba hola mundo """
+    return {
+        'Hola': 'Mundo'
+        }
+
+
+@app.get('/items/{item_id}')
+def read_item(item_id: int, q: Union[str, None] = None):
+    """ Parametro por path / y query ?, & """
+    return {
+        # Parametro de consulta
+        'irtem_id': item_id,
+        'q': q
+        }
+
+
+@app.get('/calculadora')
+def calculadora(operador_1: float, operandor_2: float):
+    """ Query params """
+    return {
+        'suma': operador_1 + operandor_2
+        }
+
